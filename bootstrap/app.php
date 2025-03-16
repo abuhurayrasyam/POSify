6edb8711d421a38778afaabaf14fd90caa348068
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-03-17 01:59:52 +0600

    Install Middleware using: php artisan inertia:middleware, after installation append the HandleInertiaRequests in app.php file, the file in bootstrap directories and the HandleInertiaRequest collect from https://inertiajs.com/server-side-setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '*',
+        ]);
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
