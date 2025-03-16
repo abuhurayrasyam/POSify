1b32b8ff85dd27419af4541896baf758eacab753
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-03-17 02:07:55 +0600

    Add the CSRF Protection in app.php file, CSRF Protection collect from: https://laravel.com/docs/12.x/csrf#main-content

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        //CSRF Protection
         $middleware->validateCsrfTokens(except: [
             '*',
         ]);
+        //CSRF Protection
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
